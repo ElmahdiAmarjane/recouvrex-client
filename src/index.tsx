47e983a82a9255e421e8fb3544635843561e76e1
ELOUAIZI Ayoub <ayoubelouaizi@gmail.com>
2024-04-18 18:29:54 +0100

    small update

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,6 @@
-// import ReactDOM from 'react-dom';
-// import { HelmetProvider } from 'react-helmet-async';
-// import { BrowserRouter } from 'react-router-dom';
-
-// // import 'nprogress/nprogress.css';
-// import App from 'src/App';
-// import { SidebarProvider } from 'src/contexts/SidebarContext';
-// import * as serviceWorker from 'src/serviceWorker';
-
-// ReactDOM.render(
-//   <HelmetProvider>
-//     <SidebarProvider>
-//       <BrowserRouter>
-//         <App />
-//       </BrowserRouter>
-//     </SidebarProvider>
-//   </HelmetProvider>,
-//   document.getElementById('root')
-// );
-
-// serviceWorker.unregister();
 import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
-// import 'nprogress/nprogress.css';
 import App from 'src/App';
 import { SidebarProvider } from 'src/contexts/SidebarContext';
 import * as serviceWorker from 'src/serviceWorker';
